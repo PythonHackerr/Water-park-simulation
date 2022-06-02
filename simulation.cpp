ce74ce9079be659bb4b82a06502e23682a0b5629
mgawlows <01169168@pw.edu.pl>
2022-06-02 06:09:40 +0200

    implemented simulation cycle

--- simulation.cpp
+++ simulation.cpp
@@ -1,5 +1,9 @@
 #include "simulation.h"
 
+// obtain a seed from the system clock:
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::mt19937 generator (seed);  // mt19937 is a standard mersenne_twister_engine
+
 Simulation::Simulation(int start_hour, int finish_hour, vector<Employee> employee_list,
     vector<WaterAttraction> attraction_list, int n_clients)
 {
@@ -46,4 +50,117 @@ int Simulation::get_n_clients() const
 vector<WaterAttraction> Simulation::get_attraction_list() const
 {
     return attraction_list;
+}
+
+string Simulation::get_cycle_message() const
+{
+    return cycle_message;
+}
+
+void Simulation::place_lifeguards()
+{
+    for(int i=0; i<employee_list.size(); i++)
+    {
+        if(employee_list[i].get_class_name() == "Lifeguard")
+        {
+            int index = generator() % attraction_list.size();
+            cout << employee_list[i] << " is guarding " << attraction_list[index] << endl;
+        }
+    }
+    cout<<endl;
+}
+
+int Simulation::get_clients_per_hour() const
+{
+    return n_clients / (finish_hour - start_hour);
+}
+
+void Simulation::get_new_clients()
+{
+    int current_n_clients = client_list.size();
+    for(int i=1; i<=get_clients_per_hour(); i++)
+    {
+        int n_cashiers = 0;
+        int n_instructors = 0;
+        for(int j=0; j<employee_list.size(); j++)
+        {
+            if(employee_list[j].get_class_name() == "Cashier")
+            {
+                n_cashiers++;
+            }
+            else if (employee_list[j].get_class_name() == "Instructor")
+            {
+                n_instructors++;
+            }
+        }
+        int cashier_number = (generator() % n_cashiers)+1;
+        int id = current_n_clients+i;
+        int duration = (generator() % (get_finish_hour() - get_current_hour())) + 1;
+        Ticket ticket = Ticket(id, duration, current_hour+duration, false);
+        int instructor_chance = generator() % 5;
+        if(instructor_chance == 0)
+        {
+            ticket.set_with_instructor(true);
+        }
+        int place_index = (generator() % attraction_list.size());
+        client_list.push_back(Client(id, ticket, attraction_list[place_index]));
+        for(int j=0; j<employee_list.size(); j++)
+        {
+            if(employee_list[j].get_class_name() == "Cashier")
+            {
+                cashier_number--;
+            }
+            if(cashier_number == 0)
+            {
+                cout << employee_list[j] << " sold ticket to " << client_list.back()<<endl;
+                break;
+            }
+        }
+        if(client_list.back().get_ticket().is_with_instructor())
+        {
+            int instructor_number = (generator() % n_instructors)+1;
+            for(int j=0; j<employee_list.size(); j++)
+            {
+                if(employee_list[j].get_class_name() == "Instructor")
+                {
+                    instructor_number--;
+                }
+                if(instructor_number==0)
+                {
+                    cout<<client_list.back()<<" went to "<<client_list.back().get_place()<<" with "<<employee_list[j]<<endl;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            cout << client_list.back() << " went to " << client_list.back().get_place() << endl;
+        }
+        cout<<endl;
+    }
+}
+
+void Simulation::cycle()
+{
+    cout<<"Current hour: "<<current_hour<<endl;
+    get_new_clients();
+    current_hour += step;
+    cout<<endl;
+}
+
+void Simulation::run()
+{
+    while (current_hour != finish_hour)
+    {
+        cycle();
+    }
+    cout<<"Current hour: "<<current_hour<<endl;
+    cout<<"Pool has closed"<<endl;
+
+}
+
+std::ostream& operator<<(std::ostream& os, const Simulation& simulation)
+{
+    os << simulation.get_cycle_message();
+    return os;
 }
--- simulation.h
+++ simulation.h
@@ -3,12 +3,16 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
+#include <chrono>
+#include <random>
 #include "employee.h"
 #include "cashier.h"
 #include "instructor.h"
 #include "lifeguard.h"
 #include "client.h"
 #include "timesheet.h"
+#include "ticket.h"
 #include "WaterAttraction.cpp"
 #include "Jacuzzi.cpp"
 #include "LanePool.cpp"
@@ -29,6 +33,7 @@ class Simulation
         vector<WaterAttraction> attraction_list;
         int n_clients;
         vector<Client> client_list;
+        string cycle_message;
 
     public:
         Simulation(int start_hour, int finish_hour, vector<Employee> employee_list,
@@ -40,5 +45,14 @@ class Simulation
         vector<Employee> get_employee_list() const;
         int get_n_clients() const;
         vector<WaterAttraction> get_attraction_list() const;
+        string get_cycle_message() const;
+
+        void place_lifeguards();
+        int get_clients_per_hour() const;
+        void get_new_clients();
+        void cycle();
+        void run();
+
+        friend std::ostream& operator<<(std::ostream& os, const Simulation& simulation);
 };
 #endif
